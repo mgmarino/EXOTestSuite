0545c87e5836a4dd0902ae6b8441d6a333f06f05
Michael Marino <mmarino@gmail.com>
2011-02-10 16:30:10 +0100

    Updating generate doc

--- generate_documentation.py
+++ generate_documentation.py
@@ -2,6 +2,8 @@ import ROOT
 import re
 
 
+boundary_limit = 100
+
 def generate_doc_for_object(obj, previous = ""):
     the_class = obj.IsA()
     list_of_everything = dir(obj)
@@ -10,10 +12,28 @@ def generate_doc_for_object(obj, previous = ""):
 
     data_list = [(member, the_class.GetDataMember(member)) for member 
                   in list_of_everything if the_class.GetDataMember(member)]
-    print previous, the_class.GetName() 
+    output = previous + '*'*10 + ' ' + the_class.GetName()
+    print output + '*'*(boundary_limit - len(output)) 
+    get_members = [getattr(obj, aname)() for aname in list_of_everything
+                  if re.match("GetNew.*", aname)]
+    to_be_created = []
     for mem, data_mem in data_list:
         # clean up title
-        print previous, "   ", mem, data_mem.GetTypeName(), data_mem.GetTitle()
+        if re.match("EXO.*", data_mem.GetTypeName()):
+            to_be_created.append(getattr(ROOT, data_mem.GetTypeName()))
+            continue
+        title = data_mem.GetTitle()
+        title = re.sub('\s{3,}', ' ', title)
+        temp = title.split(':')
+        old_value = ''
+        new_value = temp[0]
+        if len(temp) > 1:
+            old_value = temp[0]
+            new_value = temp[1]
+        print "%s%s%-40s  %-15s  %-10s %-70s" % (previous, "   ", mem, data_mem.GetTypeName(), old_value, new_value)
+    for amem in to_be_created: generate_doc_for_object(amem(), previous + "   ")
+    for amem in get_members: generate_doc_for_object(amem, previous + "   ")
+    print previous + '*'*(boundary_limit - len(previous))
 
 if __name__ == '__main__':
 
