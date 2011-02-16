b01d9e178728adcdb43f85c0990cf9972c388717
Michael Marino <mmarino@gmail.com>
2011-02-16 16:56:11 +0100

    Updating doc to output html.

--- generate_documentation.py
+++ generate_documentation.py
@@ -1,9 +1,11 @@
 import ROOT
 import re
+import datetime
 
 
-boundary_limit = 100
 
+list_type = """<ul style="list-style-type:none;">"""
+base_url = "http://dl.dropbox.com/u/979314/htmldoc/"
 def generate_doc_for_object(obj, previous = ""):
     the_class = obj.IsA()
     list_of_everything = dir(obj)
@@ -13,8 +15,7 @@ def generate_doc_for_object(obj, previous = ""):
     data_list = [(member, the_class.GetDataMember(member)) for member 
                   in list_of_everything if the_class.GetDataMember(member)]
     output = previous + '*'*10 + ' ' + the_class.GetName()
-    #print output + '*'*(boundary_limit - len(output)) 
-    print previous + the_class.GetName()
+    print """<li><a href="%s%s.html">%s</a></li>""" % (base_url, the_class.GetName(), previous + the_class.GetName())
     get_members = [getattr(obj, aname)() for aname in list_of_everything
                   if re.match("GetNew.*", aname)]
     to_be_created = []
@@ -32,14 +33,27 @@ def generate_doc_for_object(obj, previous = ""):
             old_value = temp[0]
             new_value = temp[1]
         #print "%s%s%-40s  %-15s  %-10s %-70s" % (previous, "   ", mem, data_mem.GetTypeName(), old_value, new_value)
+    if len(to_be_created)==0 and len(get_members)==0: return
+    print list_type 
     for amem in to_be_created: generate_doc_for_object(amem(), previous + "   ")
     for amem in get_members: generate_doc_for_object(amem, previous + "   ")
-    #print previous + '*'*(boundary_limit - len(previous))
+    print "</ul>"
 
 if __name__ == '__main__':
 
     ROOT.gSystem.Load("libEXOUtilities")
     ed = ROOT.EXOEventData()
+    print """
+<html>
+<body>
+<p>EXOEventData tree-structure, auto-generated on (UTC) %s</p>
+%s
+    """ % (str(datetime.datetime.utcnow()), list_type)
     generate_doc_for_object(ed)
     generate_doc_for_object(ed.GetWaveformData(), "   ")
+print """
+</ul>
+</body>
+</html>
+"""
 
--- generate_documentation_html.py
+++ generate_documentation_html.py
@@ -1,47 +1,7 @@
 import ROOT
 import re
-
-
-boundary_limit = 100
-
-def generate_doc_for_object(obj, previous = "", atitle=""):
-    the_class = obj.IsA()
-    list_of_everything = dir(obj)
-    
-    
-
-    data_list = [(member, the_class.GetDataMember(member)) for member 
-                  in list_of_everything if the_class.GetDataMember(member)]
-    output = the_class.GetName()
-    if len(atitle): output += """ (<div name="hidden" class="hidden">%s</div>)""" % atitle
-    print """<th coldiv="4" align="left">%s</th>""" % (output) 
-    get_members = [(getattr(obj, aname)(), aname.replace('New','') +"(#)", aname.replace('New', 'Num')+"s()") for aname in list_of_everything
-                  if re.match("GetNew.*", aname)]
-    to_be_created = []
-    for mem, data_mem in data_list:
-        # clean up title
-        if re.match("EXO.*", data_mem.GetTypeName()):
-            to_be_created.append((getattr(ROOT, data_mem.GetTypeName()), mem))
-            continue
-        title = data_mem.GetTitle()
-        title = re.sub('\s{3,}', ' ', title)
-        temp = title.split(':')
-        old_value = ''
-        new_value = temp[0]
-        if len(temp) > 1:
-            old_value = temp[0]
-            new_value = temp[1]
-        print """<tr><td><div name="hidden" class="hidden">%s</div>%s  </td><td>%s </td><td> %s </td><td> %s</td></tr>""" % (previous,mem, data_mem.GetTypeName(), old_value, new_value.lstrip())
-    for amem,mem in to_be_created: generate_doc_for_object(amem(), previous + mem + ".")
-    for amem,mem,title_add in get_members: generate_doc_for_object(amem, previous + mem + "->", previous + title_add)
-
-if __name__ == '__main__':
-
-    ROOT.gSystem.Load("libEXOUtilities")
-    ed = ROOT.EXOEventData()
-    print """
-<html>
-<body>
+import datetime
+"""
 <style type="text/css">
 div{ color: gray; }
 div.hidden {
@@ -64,9 +24,48 @@ div.hidden {
  </script>
 
 <a href="javascript:unhide('hidden');">Show</a>
-<table border="1">
+"""
+def generate_doc_for_object(obj, previous = "", atitle=""):
+    the_class = obj.IsA()
+    list_of_everything = dir(obj)
+    data_list = [(member, the_class.GetDataMember(member)) for member 
+                  in list_of_everything if the_class.GetDataMember(member)]
+    output = the_class.GetName()
+    if len(atitle): output += """ (<font color="gray">%s</font>)""" % atitle
+    print """<tr><th colspan="4" align="left">  %s</th></tr>""" % (output) 
+    get_members = [(getattr(obj, aname)(), aname.replace('New','') +"(#)", aname.replace('New', 'Num')+"s()") for aname in list_of_everything
+                  if re.match("GetNew.*", aname)]
+    to_be_created = []
+    for mem, data_mem in data_list:
+        # clean up title
+        if re.match("EXO.*", data_mem.GetTypeName()):
+            to_be_created.append((getattr(ROOT, data_mem.GetTypeName()), mem))
+            continue
+        title = data_mem.GetTitle()
+        title = re.sub('\s{3,}', ' ', title)
+        temp = title.split(':')
+        old_value = ' '
+        new_value = temp[0]
+        if len(temp) > 1:
+            old_value = temp[0]
+            new_value = temp[1]
+        print """<tr><td><font color="gray">%s</font>%s  </td><td>%s </td><td> %s </td><td> %s</td></tr>""" % \
+          (previous, mem, data_mem.GetTypeName(), old_value, ' ' + new_value.lstrip())
 
-    """
+    for amem,mem in to_be_created: generate_doc_for_object(amem(), previous + mem + ".")
+    for amem,mem,title_add in get_members: generate_doc_for_object(amem, previous + mem + "->", previous + title_add)
+
+if __name__ == '__main__':
+
+    ROOT.gSystem.Load("libEXOUtilities")
+    ed = ROOT.EXOEventData()
+    print """
+<html>
+<body>
+<table border="1" cellpadding="5" cellpadding="0">
+<caption>EXOEventData description, auto-generated on (UTC) %s</caption> 
+<tr><th>Name</th><th>Type</th><th>Old Name</th><th>Description</th></tr>
+    """ % (str(datetime.datetime.utcnow()))
     generate_doc_for_object(ed)
     generate_doc_for_object(ed.GetWaveformData(), "GetWaveformData()->")
     print """
